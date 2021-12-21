20cca2aaabbf83f82a2f680b4ca878c6974d9d10
olivier <olivier@evaletolab.ch>
2021-12-21 10:37:51 +0100

    new: prepare singleton for javascript context init

---
+++ cockpit/storage/singleton/config.singleton.php
@@ -0,0 +1,33 @@
+<?php
+ return array (
+  'name' => 'config',
+  'label' => 'config',
+  '_id' => 'config',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'name' => 'content',
+      'label' => '',
+      'type' => 'object',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+  ),
+  'data' => NULL,
+  '_created' => 1640077369,
+  '_modified' => 1640077369,
+  'description' => '',
+  'acl' => 
+  array (
+  ),
+);
