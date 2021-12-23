bbf1bb027eb3659bd594d7724f476dc18bbcf838
olivier <olivier@evaletolab.ch>
2021-12-23 17:36:10 +0100

    fix: remove dead code

--- cockpit/config/config.php
+++ cockpit/config/config.php
@@ -48,7 +48,6 @@ return [
             ],
             'collections' => [
                 'manage' => true,
-                'unlock' => true
             ],
             'singeltons' => [
                 'manage' => false
